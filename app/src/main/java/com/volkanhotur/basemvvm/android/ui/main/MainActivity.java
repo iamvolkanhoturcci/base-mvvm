6ef8abd9ad9a1f3d4676b981fae0d42b83340681
Volkan Hotur <volkan.hotur@cci.com.tr>
2019-11-26 14:32:28 +0300

    Alerty Dialog Added
    *Alerty Dialog library added from Github.
    *Dialog Utils class added.

--- app/src/main/java/com/volkanhotur/basemvvm/android/ui/main/MainActivity.java
+++ app/src/main/java/com/volkanhotur/basemvvm/android/ui/main/MainActivity.java
@@ -1,10 +1,15 @@
 package com.volkanhotur.basemvvm.android.ui.main;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.lifecycle.Observer;
+
 import com.volkanhotur.basemvvm.R;
 import com.volkanhotur.basemvvm.android.base.activity.BaseActivity;
+import com.volkanhotur.basemvvm.android.utils.DialogUtils;
+import com.volkanhotur.basemvvm.android.utils.impl.DialogChoiceListener;
 import com.volkanhotur.basemvvm.databinding.ActivityMainBinding;
 
 /**
@@ -29,7 +34,29 @@ public class MainActivity extends BaseActivity<MainViewModel, ActivityMainBindin
 
         viewModel.getInformationFromRemote(this);
 
-        viewModel.getInformation().observe(this, binding.data::setText);
+        viewModel.getInformation().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String message) {
+                binding.data.setText(message);
+
+
+                //Alerty Dialog Sample
+                DialogUtils.showChoiceDialog(context(), "Success",
+                        "Data fetched from Github successfully", "DONE", "CLOSE", new DialogChoiceListener() {
+                            @Override
+                            public void positiveClickListener(Dialog dialog) {
+                                DialogUtils.showInfoDialog(context(), "Success",
+                                        "Data fetched from Github successfully", "DONE");
+                            }
+
+                            @Override
+                            public void negativeClickListener(Dialog dialog) {
+                                dialog.dismiss();
+                            }
+                        });
+            }
+        });
+
     }
 
     @Override
---
+++ app/src/main/java/com/volkanhotur/basemvvm/android/utils/impl/DialogChoiceListener.java
@@ -0,0 +1,8 @@
+package com.volkanhotur.basemvvm.android.utils.impl;
+
+import android.app.Dialog;
+
+public interface DialogChoiceListener {
+    void positiveClickListener(Dialog dialog);
+    void negativeClickListener(Dialog dialog);
+}
