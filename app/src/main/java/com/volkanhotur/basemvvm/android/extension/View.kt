4dad7c962e3fcec2de24a96f103a788db30357f1
Volkan Hotur <volkan.hotur@cci.com.tr>
2020-01-22 09:56:01 +0300

    Update View Extension

--- app/src/main/java/com/volkanhotur/basemvvm/android/extension/View.kt
+++ app/src/main/java/com/volkanhotur/basemvvm/android/extension/View.kt
@@ -1,8 +1,18 @@
 package com.volkanhotur.basemvvm.android.extension
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.util.DisplayMetrics
 import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
 import android.view.animation.Animation.RELATIVE_TO_SELF
 import android.view.animation.ScaleAnimation
+import android.view.inputmethod.InputMethodManager
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import com.google.android.material.snackbar.Snackbar
 
 fun View.scaleAnimateView() {
     val animation = ScaleAnimation(1.15f, 1F, 1.15f, 1F,
@@ -10,4 +20,110 @@ fun View.scaleAnimateView() {
     this.animation = animation
     animation.duration = 100
     animation.start()
+}
+
+fun View.getString(stringResId: Int): String = resources.getString(stringResId)
+
+fun View.showKeyboard() {
+    context?.let {
+        val imm = it.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        this.requestFocus()
+        imm.showSoftInput(this, 0)
+    }
+}
+
+fun View.hideKeyboard(){
+    context?.let {
+        val inputMethodManager = it.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(windowToken, 0)
+    }
+}
+
+fun <T : ViewDataBinding> View.bind() = DataBindingUtil.bind<T>(this) as T
+
+fun View.showSnackBarLong(message: String) {
+    Snackbar.make(this, message, Snackbar.LENGTH_LONG).show()
+}
+
+fun View.showSnackBarShort(message: String) {
+    Snackbar.make(this, message, Snackbar.LENGTH_SHORT).show()
+}
+
+fun View.setPaddingLeft(value: Int) = setPadding(value, paddingTop, paddingRight, paddingBottom)
+
+fun View.setPaddingRight(value: Int) = setPadding(paddingLeft, paddingTop, value, paddingBottom)
+
+fun View.setPaddingTop(value: Int) = setPaddingRelative(paddingStart, value, paddingEnd, paddingBottom)
+
+fun View.setPaddingBottom(value: Int) = setPaddingRelative(paddingStart, paddingTop, paddingEnd, value)
+
+fun View.setHeight(value: Int) {
+    val lp = layoutParams
+    lp?.let {
+        lp.height = value
+        layoutParams = lp
+    }
+}
+
+fun View.setWidth(value: Int) {
+    val lp = layoutParams
+    lp?.let {
+        lp.width = value
+        layoutParams = lp
+    }
+}
+
+fun View.show() : View {
+    if (visibility != View.VISIBLE) {
+        visibility = View.VISIBLE
+    }
+    return this
+}
+
+fun View.hide() : View {
+    if (visibility != View.INVISIBLE) {
+        visibility = View.INVISIBLE
+    }
+    return this
+}
+
+fun View.getBitmap(): Bitmap {
+    val bmp = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+    val canvas = Canvas(bmp)
+    draw(canvas)
+    canvas.save()
+    return bmp
+}
+
+fun View.getScreenHeight(): Int {
+    context?.let {
+        val windowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        val dm = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(dm)
+        return dm.heightPixels
+    }.run{
+        return 0
+    }
+}
+
+fun View.getScreenWidth(): Int {
+    context?.let {
+        val windowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        val dm = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(dm)
+        return dm.widthPixels
+    }.run {
+        return 0
+    }
+}
+
+fun View.getStatusBarHeight(): Int {
+    resources?.let {
+        val resourceId = resources.getIdentifier("status_bar_height", "dimen", "android")
+        if (resourceId > 0) {
+            return resources.getDimensionPixelSize(resourceId)
+        }
+    }
+
+    return 0
 }
